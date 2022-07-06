60eebc8e4df96b0eb859f25fcb0aa036c6108873
Guillaume <guillaumelamarche1@gmail.com>
2022-07-06 11:11:19 -0400

    inital setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyle from "global.style";
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/*Global style is applied here*/}
+      <GlobalStyle />
+      <div className="App">
+        <h1>Si ça marche ce texte sera rouge</h1>
+      </div>
+    </>
   );
 }
 
