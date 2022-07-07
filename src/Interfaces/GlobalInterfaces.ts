99f92fd18e4476018b97e400d81b227d246cf270
Guillaume <guillaumelamarche1@gmail.com>
2022-07-07 17:00:57 -0400

    chore: create interfaces for recipe and instruction

--- src/Interfaces/GlobalInterfaces.ts
+++ src/Interfaces/GlobalInterfaces.ts
@@ -1,33 +1,20 @@
 export interface IRecipe {
-  id: number;
-  name: string;
-  description: string;
-  image: string;
-  ingredients: IIngredient[];
-  instructions: string[];
-  category: string;
-  date: string;
-  user: IUser;
-}
-
-export interface IIngredient {
-  id: number;
   name: string;
   image: string;
-  description: string;
-  quantity: number;
-}
-
-export interface IUser {
-  id: number;
-  name: string;
-  email: string;
-  image: string;
+  description?: string; //A short summary describing the dish.
+  prepTime?: number;
+  cookTime?: number;
+  recipeIngredient?: string[];
+  recipeInstructions?: IInstructions[];
+  recipeCategory?: string; //The type of meal or course your recipe is about. For example: "dinner", "main course", or "dessert, snack".
+  datePublished?: string;
+  recipeYield?: string | number; // The quantity produced by the recipe. Specify the number of servings produced from this recipe with just a number.
 }
 
 export interface IInstructions {
-  id: number;
-  name: string;
-  description: string;
-  step: number;
+  type: "HowToStep";
+  text: string; //The full instruction text of this step
+  name?: string; //The word or short phrase summarizing the step
+  url?: string; //A URL that directly links to the step (if one is available). For example, an anchor link fragment.
+  image?: string; //An image for the step.
 }
