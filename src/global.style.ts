80741bb08f95aa274c6e2c2896797826dee6a746
Nicolas Nadeau <nico.pou@hotmail.com>
2022-07-08 13:25:08 -0400

    test: red

--- src/global.style.ts
+++ src/global.style.ts
@@ -7,6 +7,7 @@ const GlobalStyle = createGlobalStyle`
        outline:0;
        box-sizing:border-box;
        font-family: 'Open Sans', sans-serif;
+       color: red;
    }
    #root{
        margin:0 auto;
