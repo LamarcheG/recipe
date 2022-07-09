33c2eb5d7fbec417915c1d81494e83e7365219d0
Guillaume <guillaumelamarche1@gmail.com>
2022-07-09 15:14:05 -0400

    feat:extract recipe form html

--- src/Utility/Scrapper.ts
+++ src/Utility/Scrapper.ts
@@ -1,8 +1,22 @@
-export const getRecipe = async (url: string) => {
-  const response = await fetch(url, {
-    method: "GET",
-    headers: { "Content-Type": "application/json", Accept: "application/json" },
-  });
+const getRecipe = async (url: string) => {
+  const response = await fetch(url);
   const data = await response.text();
   return data;
+};
+
+const extractRecipe = (html: string) => {
+  const startTag = '<script type="application/ld+json">';
+  const endTag = "</script>";
+  const startTagLength = startTag.length;
+
+  const start = html.indexOf(startTag) + startTagLength;
+  const end = html.indexOf(endTag, start);
+  const json = html.substring(start, end);
+  const recipe = JSON.parse(json);
+  console.log(recipe);
+};
+
+export const scrapper = async (url: string) => {
+  const html = await getRecipe(url);
+  extractRecipe(html);
 };
